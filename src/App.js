ea28fa99998c75e4116093e6c71eef1dea08cb1d
Denys Chornyi <chornijj14@gmail.com>
2021-07-20 18:23:37 +0200

    git init nex project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import List from "./components/List";
+import listSvg from "./assets/img/list.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      <div className="todo__sidebar">
+        <List
+          items={[
+            {
+              icon: <img src={listSvg} alt="All tasks" />,
+              name: "All tasks",
+              active: true
+            },
+            { color: "green", name: "Frontend" },
+            { color: "blue", name: "Daily basis" },
+            { color: "pink", name: "Movies and series" }
+          ]}
+        />
+      </div>
+      <div className="todo__tasks"></div>
     </div>
   );
 }
